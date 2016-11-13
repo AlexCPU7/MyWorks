4c384d79676157c19f0bb9d286017ad3f187a808
AlexCPU7 <alexcpu7@mail.ru>
2016-11-13 17:17:08 +0300

    del

--- JavaScript/JavaScriptCodecademy/main.js
+++
@@ -1,5 +0,0 @@
-if (10 === 10)(
-         console.log("Верно!");
-) else (
-    console.log("Неверно!");
-)
--- JavaScript/RandomNumber/Random.js
+++
@@ -1,7 +0,0 @@
-var a;
-var b;
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-alert(getRandomInt(1, 3))
